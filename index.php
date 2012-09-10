cb5836ccfb88bf0c951cca876911e518940fbb21
Pascal Gagneur <pascal.gagneur@postkodlotteriet.se>
2012-09-11 00:14:59 +0200

    Navigation almost finished Form need som styles

--- index.php
+++ index.php
@@ -56,8 +56,8 @@ if($_GET['q']) {
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width">
 
-    <!--<link rel="stylesheet" href="css/normalize.min.css">
-    <link rel="stylesheet" href="css/main.css">-->
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/main.css">
 
     <script src="js/vendor/modernizr-2.6.1.min.js"></script>
     <!-- live reload snippet-->
@@ -66,46 +66,68 @@ if($_GET['q']) {
 <body>
 <div class="header-container">
     <header class="wrapper clearfix">
-        <h1 class="title ir">logo</h1>
-        <nav>
+        <h1 class="title ir" title="Company name">Company name</h1>
+        <nav class="main-navigation">
             <ul role="navigation">
-                <li><a href="#">nav ul li a</a></li>
-                <li><a href="#">nav ul li a</a></li>
-                <li><a href="#">nav ul li a</a></li>
-                <li><a href="#">nav ul li a</a></li>
+                <li class="nav"><a href="#" class="nav">Home<br />Service Description</a></li>
+                <li class="nav"><a href="#" class="nav">Our aim<br />Service Description</a></li>
+                <li class="nav"><a href="#" class="nav">About us<br />Service Description</a></li>
+                <li class="nav"><a href="#" class="nav">Contact<br />Service Description</a></li>
             </ul>
         </nav>
     </header>
 </div>
-<div class="main-container">
-    <div class="main wrapper clearfix">
-
-        <article>
-            <header>
-                <h1>article header h1</h1>
+<div class="main-container" role="main">
+    <div class="wrapper clearfix">
+        <section class="banner-container clearfix">
+            <article class="banner">
+                    <img src="img/banner1.jpg" alt="some info">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ante enim, tempor quis, consectetur in, sollicitudin a, nibh. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+            </article>
+            <article class="banner">
+                <img src="img/banner1.jpg" alt="some info">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec.</p>
-            </header>
-            <section>
-                <h2>article section h2</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
-            </section>
-            <section>
-                <h2>article section h2</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
-            </section>
-            <footer>
-                <h3>article footer h3</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor.</p>
-            </footer>
-        </article>
+            </article>
+        </section>
+        <section class="content-container clearfix">
+                <div class="content">
+                    <h1>
+                        Welcome To Our Site
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut mauris tIn a orci non turpis elemInteger ornare nisl in tortor. Curabitur accumsan erat ut lorem. Sed iaculis commodo erat. Curaest dui, lacinia vitae, adipiscing vitae, feugiat sit amet, tortor
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut mauris tIn a orci non turpis elemInteger ornare nisl in tortor. Curabitur accumsan erat ut lorem. Sed iaculis commodo erat. Curaest dui, lacinia vitae, adipiscing vitae, feugiat sit amet, tortor
+                    </p>
+               </div>
+        </section>
+    </div>
+        <section class="company-info clearfix">
+        <div class="wrapper">
 
-        <aside>
-            <h3>aside</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
-        </aside>
+            <article >
+                    <a><h2 class="icon-home">Lorme Ipsom Dolor</h2></a>
+                    <p>Lorem Ipsum is simply dummy text of the
+                        printing and typesetting industry. Lorem Ip unknown printer took a galley of type and</p>
+                    <a>see more..</a>
+            </article>
+            <article  >
+                <a><h2 class="icon-paper">Lorme Ipsom Dolor</h2></a>
+                    <p>Lorem Ipsum is simply dummy text of the
+                        printing and typesetting industry. Lorem Ip unknown printer took a galley of type and</p>
+                    <a>see more..</a>
+            </article>
 
-    </div> <!-- #main -->
-    <section class="wrapper">
+            <article class="last">
+                <a><h2 class="icon-bag">Lorme Ipsom Dolor</h2></a>
+                    <p>Lorem Ipsum is simply dummy text of the
+                        printing and typesetting industry. Lorem Ip unknown printer took a galley of type and</p>
+                    <a>see more..</a>
+            </article>
+        </div>
+    </section>
+    <section class="quick-contact wrapper clearfix">
         <form action="index.php" role="search" method="get" name="quickcontact">
             <input type="hidden" name="q" value="save">
             <fieldset>
@@ -115,7 +137,7 @@ if($_GET['q']) {
                 <label for="email" class="visuallyhidden">e-mail</label>
                 <input id="email" type="email" name="<?=EMAIL?>" placeholder="e-mail" title="e-mail" required="required" />
                 <label for="website" class="visuallyhidden" >Website <abbr title="Uniform resource locator">URL</abbr></label>
-                <input id="website" name="<?=WEBSITE?>" type="url" name="url" placeholder="Website URL" title="Website URL" required="required" />
+                <input id="website" name="<?=WEBSITE?>" type="url" placeholder="Website URL" title="Website URL" required="required" />
                 <label for="msg" class="visuallyhidden">Write your message here</label>
                 <textarea id="msg" name="<?=MSG?>" required="required" placeholder="Write your message here" title="Write your message here."></textarea>
                 <input type="submit" title="Submit">
@@ -125,7 +147,7 @@ if($_GET['q']) {
 </div> <!-- #main-container -->
 <div class="footer-container">
     <footer class="wrapper">
-        <h3>footer</h3>
+        &copy;<span class="rss">RSS</span>
     </footer>
 </div>
 
